63862b590c72476922f0668896cb623607a7add0
lakshmisharath31 <62305053+lakshmisharath31@users.noreply.github.com>
2020-08-03 22:46:04 -0400

    Update SearchController.java

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -16,25 +16,29 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
+    public String title;
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
     @RequestMapping(value = "results")
-    public String search(@RequestParam String searchType, @RequestParam String searchTerm, Model model) {
+    public String search(@RequestParam String searchType, @RequestParam String searchTerm,String title, Model model) {
+         title="Jobs With "+searchType+" : " +searchTerm;
         ArrayList<Job> jobs;
         if (searchType.equals("all")) {
             jobs = JobData.findByValue(searchTerm);
         }
         else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title",title);
             model.addAttribute("selectedColumn", searchType);
         }
-
+        model.addAttribute("searchType",searchType);
+        model.addAttribute("searchTerm",searchTerm);
         model.addAttribute("columns", ListController.columnChoices);
         model.addAttribute("jobs", jobs);
+        model.addAttribute("title",title);
 
         return "search";
     }
